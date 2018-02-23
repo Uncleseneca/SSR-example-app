e9d738c34e60a6b99eb7edc8dc3f53b6897c0d6b
Vladimir Golotin <uncleseneca@gmail.com>
2018-02-23 16:55:48 +0300

    added players animation

--- src/components/Players.js
+++ src/components/Players.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { parse } from 'query-string';
 import slug from 'slug';
 import { Route, Link } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import { getPlayers } from '../api';
 import Sidebar from './Sidebar';
@@ -54,40 +55,44 @@ class Players extends React.Component {
  name, position, teamId, number, avatar, apg, ppg, rpg, spg,
 } = players.find(player => slug(player.name) === match.params.playerId);
             return (
-              <div className="panel">
-                <img className="avatar" src={`${avatar}`} alt={`${name}'s avatar`} />
-                <h1 className="medium-header">{name}</h1>
-                <h3 className="header">#{number}</h3>
-                <div className="row">
-                  <ul className="info-list" style={{ marginRight: 80 }}>
-                    <li>
-                      Team
-                      <div>
-                        <Link style={{ color: '#68809a' }} to={`/${teamId}`}>
-                          {teamId[0].toUpperCase() + teamId.slice(1)}
-                        </Link>
-                      </div>
-                    </li>
-                    <li>
-                      Position<div>{position}</div>
-                    </li>
-                    <li>
-                      PPG<div>{ppg}</div>
-                    </li>
-                  </ul>
-                  <ul className="info-list">
-                    <li>
-                      APG<div>{apg}</div>
-                    </li>
-                    <li>
-                      SPG<div>{spg}</div>
-                    </li>
-                    <li>
-                      RPG<div>{rpg}</div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <TransitionGroup className="panel">
+                <CSSTransition key={location.key} timeout={250} classNames="fade">
+                  <div className="panel">
+                    <img className="avatar" src={`${avatar}`} alt={`${name}'s avatar`} />
+                    <h1 className="medium-header">{name}</h1>
+                    <h3 className="header">#{number}</h3>
+                    <div className="row">
+                      <ul className="info-list" style={{ marginRight: 80 }}>
+                        <li>
+                          Team
+                          <div>
+                            <Link style={{ color: '#68809a' }} to={`/${teamId}`}>
+                              {teamId[0].toUpperCase() + teamId.slice(1)}
+                            </Link>
+                          </div>
+                        </li>
+                        <li>
+                          Position<div>{position}</div>
+                        </li>
+                        <li>
+                          PPG<div>{ppg}</div>
+                        </li>
+                      </ul>
+                      <ul className="info-list">
+                        <li>
+                          APG<div>{apg}</div>
+                        </li>
+                        <li>
+                          SPG<div>{spg}</div>
+                        </li>
+                        <li>
+                          RPG<div>{rpg}</div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </CSSTransition>
+              </TransitionGroup>
             );
           }}
         />
